52d7e37bf3bac56011dbee97d9196118f3d81221
byelBorges <gabryelhenborges@gmail.com>
2023-12-07 22:16:17 -0300

    implementacao toast

--- src/telasCadastro/tabelas/TabelaCategorias.jsx
+++ src/telasCadastro/tabelas/TabelaCategorias.jsx
@@ -1,50 +1,81 @@
 import { Container, Table, Button } from "react-bootstrap";
-import {useSelector, useDispatch} from "react-redux";
-import { removerCategoria } from "../../redux/categoriaReducer";
+import { useSelector, useDispatch } from "react-redux";
+import { buscarCategorias, removerCategoria } from "../../redux/categoriaReducer.js";
+import ESTADO from "../../redux/recursos/estado.js";
+import toast from "react-toastify";
+import { useEffect } from "react";
 export default function TabelaCategorias(props) {
-    const {status, mensagem, listaCategorias}= useSelector((state)=>state.categoria);
+    const { estado, mensagem, listaCategorias } = useSelector((state) => state.categoria);
     const dispatch = useDispatch();
-    function excluirCategoria(cat){
-        if(window.confirm('Deseja excluir esta categoria?'))
+    function excluirCategoria(cat) {
+        if (window.confirm('Deseja excluir esta categoria?'))
             dispatch(removerCategoria(cat));
     }
 
-    return (
-        <Container>
-            <Button type="button" onClick={()=>{
-                props.exibirFormulario(true);
-            }}>Nova Categoria</Button>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Categoria</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        listaCategorias.map((categoria) => {
-                            return (<tr key={categoria.nome}>
-                                <td>{categoria.nome}</td>
-                                <td>
-                                    <Button variant="danger" onClick={() => {
-                                        excluirCategoria(categoria);
-                                    }}>
-                                        <svg xmlns="http://www.w3.org/2000/svg"
-                                            width="16"
-                                            height="16"
-                                            fill="currentColor"
-                                            className="bi bi-trash"
-                                            viewBox="0 0 16 16">
-                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
-                                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
-                                        </svg>
-                                    </Button>
-                                </td>
-                            </tr>);
-                        })
-                    }
-                </tbody>
-            </Table>
-        </Container>
-    );
+    function editarCategoria(cat) {
+        props.setCategoriaParaEdicao(cat);
+        props.setModoEdicao(true);
+        props.exibirFormulario(true);
+    }
+
+    useEffect(() => {
+        dispatch(buscarCategorias());
+    }, [dispatch]);
+
+    if (estado === ESTADO.PENDENTE) {
+        toast(({ closeToast }) =>
+            <div>
+                <Spinner animation="border" role="status"></Spinner>
+                <p>Buscando categorias....</p>
+            </div>
+            , { toastId: estado });
+    }
+    else if (estado === ESTADO.ERRO) {
+        toast.error(({ closeToast }) =>
+            <div>
+                <p>{mensagem}</p>
+
+            </div>
+            , { toastId: estado });
+    }
+    else {
+        return (
+            <Container>
+                <Button type="button" onClick={() => {
+                    props.exibirFormulario(true);
+                }}>Nova Categoria</Button>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Categoria</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            listaCategorias.map((categoria) => {
+                                return (<tr key={categoria.nome}>
+                                    <td>{categoria.nome}</td>
+                                    <td>
+                                        <Button variant="danger" onClick={() => {
+                                            excluirCategoria(categoria);
+                                        }}>
+                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                width="16"
+                                                height="16"
+                                                fill="currentColor"
+                                                className="bi bi-trash"
+                                                viewBox="0 0 16 16">
+                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
+                                                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+                                            </svg>
+                                        </Button>
+                                    </td>
+                                </tr>);
+                            })
+                        }
+                    </tbody>
+                </Table>
+            </Container>
+        );
+    }
 }
